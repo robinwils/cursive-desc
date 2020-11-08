a1e12f7aab9b5939ba180cc96ddf337907a99ddf
Robin Wils <rwils@mail.corp.sentryo.net>
2020-11-09 00:30:12 +0100

    callback_macro: rename generation function

--- cursive_callback_macro/src/lib.rs
+++ cursive_callback_macro/src/lib.rs
@@ -9,7 +9,7 @@ use syn::{
     braced, parse_macro_input, token, Field, Ident, ImplItem, ItemImpl, Result, Token, Type,
 };
 
-fn generate_map_insert(item: &ImplItem) -> Option<proc_macro2::TokenStream> {
+fn generate_cb_pattern_match(item: &ImplItem) -> Option<proc_macro2::TokenStream> {
     match item {
         ImplItem::Method(method_item) => {
             let method_name = method_item.sig.ident.clone();
@@ -33,7 +33,7 @@ pub fn cursive_callbacks(attr: TokenStream, item: TokenStream) -> TokenStream {
     let map_inserts = cb_impl
         .items
         .iter()
-        .map(|item| generate_map_insert(item))
+        .map(|item| generate_cb_pattern_match(item))
         .filter(|x| x.is_some());
 
     if let Some(name) = struct_name {
